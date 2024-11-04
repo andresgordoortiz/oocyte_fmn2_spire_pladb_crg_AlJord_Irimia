dd8f4df3d48d87613b98b55c01db94e3da980f8b
Andrés Gordo <andresgordoortiz@gmail.com>
2024-11-04 13:18:19 +0100

    Add gene ontology output for Mm2

--- tests/workflow_tests/vast_compare_docker.sh
+++ tests/workflow_tests/vast_compare_docker.sh
@@ -3,4 +3,6 @@ docker run -v $(pwd)/tests/data_tests/:/usr/local/vast-tools/share vast-tools ba
     -a Oocytes_FG_Spire12_Cont_a,Oocytes_FG_Spire12_Cont_b,Oocytes_FG_Spire12_Cont_c \
     -b Oocytes_FG_Spire12_DKO_a,Oocytes_FG_Spire12_DKO_b,Oocytes_FG_Spire12_DKO_c \
     --min_dPSI 25 \
-    --min_range 5 > summary_stats.txt"
+    --min_range 5 \
+    --GO \
+    -sp Mm2 > summary_stats.txt"
