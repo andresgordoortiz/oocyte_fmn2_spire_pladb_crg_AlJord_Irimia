85ff5e2fe12ccd9132ab48e4262cd511a163db0d
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-07 09:38:28 +0100

    Add missing 'logs' directory to project folder structure creation

--- project_folder_structure.sh
+++ project_folder_structure.sh
@@ -2,7 +2,7 @@
 
 
 # Create directories
-mkdir -p {config/{docker},data/{raw,processed,metadata},notebooks/{exploratory,final,archive},scripts/{R,python,bash},workflows/modules,results/{tables,figures,logs},docs,tests/{data_tests,workflow_tests},.github/workflows}
+mkdir -p {config/{docker},data/{raw,processed,metadata},notebooks/{exploratory,final,archive},scripts/{R,python,bash},workflows/modules,results/{tables,figures,logs},docs,tests/{data_tests,workflow_tests},.github/workflows, logs}
 
 # Notify completion
 echo "Folder structure created successfully."
