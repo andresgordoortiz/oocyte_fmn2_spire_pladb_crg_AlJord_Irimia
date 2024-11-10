49e59204fbfa5d4e7638ab402f28f9aea6a1b29a
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-10 21:43:15 +0100

    Add SLURM job script for VAST alignment with logging and resource configuration

---
+++ scripts/bash/vast_align.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=60
+
+# queue
+#SBATCH --qos=shorter
+
+# memory (MB)
+#SBATCH --mem=10G
+#SBATCH --cpus-per-task=8
+
+# job name
+#SBATCH --job-name vast-align
+# job array directive
+#SBATCH --array=0-3
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+
+###############
+# run command #
+###############
+
+#Define file list and select the file for the current array job
+files=($PWD/data/processed/fmn2dko/*.fq.gz)
+file=${files[$SLURM_ARRAY_TASK_ID]}
+
+basename=$(basename "$file" .fq.gz)
+
+singularity exec --bind $PWD/data/processed/fmn2dko/vast_out --bind /users/mirimia/projects/vast-tools/VASTDB/:/VASTDB docker://vastgroup/vast-tools:latest \
+    vast-tools align \
+    "$file" \
+    -sp mm10 \
+    -o $PWD/data/processed/fmn2dko/vast_out \
+    --expr \
+    --IR_version 2 \
+    -c 8 \
+    -n "$basename"
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
