123dc4f2b2b71d4b84cd16fe0f7106c823b81197
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-10 18:25:49 +0100

    Add multiqc.sh script for running MultiQC on trimmed FASTQ files

---
+++ scripts/bash/multiqc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=1
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=200
+
+# job name
+#SBATCH --job-name multiqc
+
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+################
+# run multiqc  #
+################
+cd $PWD/downloads/trimmed
+module load MultiQC/1.22.3-foss-2023b
+multiqc .
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
