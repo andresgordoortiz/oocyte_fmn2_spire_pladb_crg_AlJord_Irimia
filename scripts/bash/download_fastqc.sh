ac78cbe082dbe0b351a63ee20294f0247e6fcd18
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-10 13:16:45 +0100

    Add download_fastqc.sh script for SLURM job submission and file downloading

---
+++ scripts/bash/download_fastqc.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=60
+
+# queue
+#SBATCH --qos=short
+
+# memory (MB)
+#SBATCH --mem=5G
+
+# job name
+#SBATCH --job-name downloadfasta
+
+# job array directive
+#SBATCH --array=0-$(($(wc -l < "$1") - 1))
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+###############
+# run command #
+###############
+wget $(sed "$((SLURM_ARRAY_TASK_ID + 1))q;d" "$1")
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
+
+#####################
+# submit fastqc job #
+#####################
+if [ "$SLURM_ARRAY_TASK_ID" -eq "$(($(wc -l < "$1") - 1))" ]; then
+  sbatch --dependency=afterok:$SLURM_JOB_ID run_fastqc.sh
+fi
