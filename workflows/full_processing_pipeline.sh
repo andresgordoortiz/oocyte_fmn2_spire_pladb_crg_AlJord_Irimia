43648b3054c17ccf20de1a0657a23b0a10d80451
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-10 18:26:08 +0100

    Add full_processing_pipeline.sh to orchestrate job submissions for FASTQ processing

---
+++ workflows/full_processing_pipeline.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# run a pipeline made up of a sequence of 2 separate jobs
+
+# first job - process fastqs to find kmers
+echo submitting first job...
+jid1=$(sbatch $PWD/scripts/bash/processing1_cat_reads.sh | tr -cd '[:digit:].')
+echo ...first job id is $jid1
+
+# second job - collate partial answers from first job into
+echo submitting second job...
+jid2=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/processing2_trim_reads.sh | tr -cd '[:digit:].')
+echo ...second job id is $jid2
+
+echo submitting third job...
+jid3=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/multiqc.sh | tr -cd '[:digit:].')
+echo ...second job id is $jid3
