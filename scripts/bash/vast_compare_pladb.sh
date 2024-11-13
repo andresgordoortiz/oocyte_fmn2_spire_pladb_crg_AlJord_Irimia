a1b1bd35fc156db72df9c365b5da10f27fdfd503
andresgordoortiz <andresgordoortiz@gmail.com>
2024-11-13 11:27:21 +0100

    Update vast_compare_pladb script to replace hyphens with underscores in group names and adjust minimum parameters

--- scripts/bash/vast_compare_pladb.sh
+++ scripts/bash/vast_compare_pladb.sh
@@ -38,8 +38,8 @@ set -o pipefail
 ###############
 # run command #
 ###############
-group_a="2022-038_S10_L001_R1_001_merged_trimmed,2022-039_S11_L001_R1_001_merged_trimmed,2022-040_S12_L001_R1_001_merged_trimmed"
-group_b="2022-044_S16_L001_R1_001_merged_trimmed,2022-045_S17_L001_R1_001_merged_trimmed,2022-046_S18_L001_R1_001_merged_trimmed"
+group_a="2022_038_S10_L001_R1_001_merged_trimmed,2022_039_S11_L001_R1_001_merged_trimmed,2022_040_S12_L001_R1_001_merged_trimmed"
+group_b="2022_044_S16_L001_R1_001_merged_trimmed,2022_045_S17_L001_R1_001_merged_trimmed,2022_046_S18_L001_R1_001_merged_trimmed"
 name_a="Control"
 name_b="PladB_treatment_10mM"
 
@@ -52,9 +52,9 @@ cd $PWD/data/processed/pladienolideb/vast_out
 /users/mirimia/projects/vast-tools/vast-tools compare INCLUSION_LEVELS_FULL-mm10-9.tab \
     -a $group_a \
     -b $group_b \
-    --min_dPSI 20 \
-    --min_range 5 \
-    --GO --print_dPSI --print_sets \
+    --min_dPSI 0 \
+    --min_range 0 \
+    --GO --print_dPSI --print_sets ---print_AS_ev \
     -name_A $name_a  -name_B $name_b \
     -sp mm10 > summary_stats_control_vs_pladb10mM.txt
 
