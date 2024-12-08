32759710f876c931b90ffa4019a54e9026274a34
{your_name} <andresgordoortiz@gmail.com>
2024-12-08 17:47:55 +0100

    Add script for downloading FMNDKO study data with SLURM settings

---
+++ scripts/bash/fmndko_study/downloading_fmndko.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=3
+
+# queue
+#SBATCH --qos=vshort
+
+# memory (MB)
+#SBATCH --mem=5G
+
+# job name
+#SBATCH --job-name downloadfasta
+
+#SBATCH --array=0-11
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
+
+# Trap errors and print a message
+trap 'echo [$(date +"%Y-%m-%d %H:%M:%S")] "An error occurred. Exiting..."' ERR
+
+
+###############
+# run command #
+###############
+SAMPLES_FILE=$1
+PROJECT_NAME=$2
+echo "Project name is set to $PROJECT_NAME"
+
+mkdir -p $PWD/data/raw/$PROJECT_NAME
+
+echo "Changing directory to $PWD/data/raw/$PROJECT_NAME"
+CURRENT_DIR=$PWD
+cd $PWD/data/raw/$PROJECT_NAME
+
+echo "Running command from file:"
+sed "$((SLURM_ARRAY_TASK_ID + 1))q;d" $CURRENT_DIR/$SAMPLES_FILE | bash
+
+###############
+# end message #
+###############
+end_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] finished on $(hostname) after $((end_epoch-start_epoch)) seconds
