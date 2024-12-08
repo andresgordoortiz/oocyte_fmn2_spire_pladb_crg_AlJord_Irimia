dc43a04786f9d57fc56cba0320f28d2bb0ce1a01
{your_name} <andresgordoortiz@gmail.com>
2024-12-08 18:10:36 +0100

    Update full processing pipeline script to download dataset from ENA and adjust job dependencies

--- workflows/full_processing_pipeline_fmndko.sh
+++ workflows/full_processing_pipeline_fmndko.sh
@@ -22,29 +22,35 @@ if [ -z "$1" ]; then
 fi
 
 VASTDB_PATH=$1
-# First job - concatenate reads
-echo "Submitting first job: Concatenate reads..."
-jid1=$(sbatch $PWD/scripts/bash/pladb_study/processing1_cat_reads_pladb.sh | tr -cd '[:digit:].')
+
+# Downlaoding Dataset from ENA
+echo "Submitting first job: Downloading reads from ENA..."
+jid1=$(sbatch $PWD/scripts/bash/fmndko_study/downloading_fmndko.sh | tr -cd '[:digit:].')
 echo "...first job ID is $jid1"
+
+# Second job - concatenate reads
+echo "Submitting first job: Concatenate reads..."
+jid2=$(sbatch $PWD/scripts/bash/pladb_study/processing1_cat_reads_pladb.sh | tr -cd '[:digit:].')
+echo "...first job ID is $jid2"
 
 # Second job - trim reads (dependent on first job)
 echo "Submitting second job: Trim reads..."
-jid2=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/pladb_study/processing2_trim_reads_pladb.sh | tr -cd '[:digit:].')
-echo "...second job ID is $jid2"
+jid3=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/pladb_study/processing2_trim_reads_pladb.sh | tr -cd '[:digit:].')
+echo "...second job ID is $jid3"
 
 # Third job - align reads (dependent on second job)
 echo "Submitting third job: Align reads..."
-jid3=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/pladb_study/vast_align_pladb.sh $VASTDB_PATH | tr -cd '[:digit:].')
-echo "...third job ID is $jid3"
+jid4=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/pladb_study/vast_align_pladb.sh $VASTDB_PATH | tr -cd '[:digit:].')
+echo "...third job ID is $jid4"
 
 # Fourth job - generate multiQC report (dependent on third job)
 echo "Submitting fourth job: Generate multiQC report..."
-jid4=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/multiqc.sh | tr -cd '[:digit:].')
-echo "...fourth job ID is $jid4"
+jid5=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/multiqc.sh | tr -cd '[:digit:].')
+echo "...fourth job ID is $jid5"
 
 # Fifth job - run vast combine (dependent on third job)
 echo "Submitting fifth job: Run vast combine..."
-jid5=$(sbatch --dependency=afterok:$jid3 $PWD/scripts/bash/pladb_study/vast_combine_pladb.sh $VASTDB_PATH | tr -cd '[:digit:].')
-echo "...fifth job ID is $jid5"
+jid6=$(sbatch --dependency=afterok:$jid4 $PWD/scripts/bash/pladb_study/vast_combine_pladb.sh $VASTDB_PATH | tr -cd '[:digit:].')
+echo "...fifth job ID is $jid6"
 
 echo "All jobs submitted!"
