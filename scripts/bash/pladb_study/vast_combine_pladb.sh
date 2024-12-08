fca7c0b4c7f56b67276c6fabd457fdcff6c6f59b
{your_name} <andresgordoortiz@gmail.com>
2024-12-08 19:06:25 +0100

    Update vast_combine_pladb.sh to specify output directory for combined results

--- scripts/bash/pladb_study/vast_combine_pladb.sh
+++ scripts/bash/pladb_study/vast_combine_pladb.sh
@@ -48,7 +48,7 @@ singularity_image="docker://andresgordoortiz/vast-tools:latest"
 # Run vast-tools align using Singularity
 singularity exec --bind $VASTDB_PATH:/usr/local/vast-tools/VASTDB \
     --bind $PWD/data/processed/pladb:/pladb \
-    $singularity_image bash -c "vast-tools combine /pladb -sp mm10"
+    $singularity_image bash -c "vast-tools combine /pladb/vast_out/to_combine -sp mm10 -o /pladb/vast_out"
 
 ###############
 # end message #
