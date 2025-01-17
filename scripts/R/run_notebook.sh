20efdf49c28bebe70d000eaa6aa5fb8e4420341f
andresgordoortiz <andresgordoortiz@gmail.com>
2025-01-17 15:37:22 +0100

    Update SLURM script for R notebook: increase time limit, memory, and CPU allocation; change rendered RMarkdown file

--- scripts/R/run_notebook.sh
+++ scripts/R/run_notebook.sh
@@ -10,14 +10,14 @@
 #SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
 
 # time limit in minutes
-#SBATCH --time=90
+#SBATCH --time=360
 
 # queue
 #SBATCH --qos=short
 
 # memory (MB)
-#SBATCH --mem=15G
-#SBATCH --cpus-per-task=2
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8
 
 # job name
 #SBATCH --job-name run_RMarkdown
@@ -54,7 +54,7 @@ fi
 # Set the working directory inside the container to /workspace
 singularity run --bind "$(pwd)/notebooks:/shared" \
   docker://andresgordoortiz/splicing_analysis_r_crg:v1.3 \
-  bash -c "cd /; Rscript -e \"rmarkdown::render('/shared/oocyte_transcript_analysis.rmd')\""
+  bash -c "cd /; Rscript -e \"rmarkdown::render('/shared/oocyte_fmndko_spire_versionD.Rmd')\""
 
 mkdir -p $PWD/results/tables
 mv $PWD/notebooks/*.csv $PWD/results/tables
