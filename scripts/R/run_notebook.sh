3ddf5345537f64ddd76187e8347a023eb989d2dd
Andrés Gordo <andresgordoortiz@gmail.com>
2025-03-28 21:01:15 +0100

    Modify run time

--- scripts/R/run_notebook.sh
+++ scripts/R/run_notebook.sh
@@ -10,14 +10,14 @@
 #SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
 
 # time limit in minutes
-#SBATCH --time=360
+#SBATCH --time=2880
 
 # queue
-#SBATCH --qos=short
+#SBATCH --qos=vlong
 
 # memory (MB)
 #SBATCH --mem=50G
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=2
 
 # job name
 #SBATCH --job-name run_RMarkdown
@@ -53,11 +53,10 @@ else
 fi
 # Set the working directory inside the container to /workspace
 singularity run --bind "$(pwd)/notebooks:/shared" \
-  docker://andresgordoortiz/splicing_analysis_r_crg:v1.3 \
-  bash -c "cd /; Rscript -e \"rmarkdown::render('/shared/oocyte_fmndko_spire_versionD.Rmd')\""
+  docker://andresgordoortiz/splicing_analysis_r_crg:v1.5 \
+  bash -c "cd /; Rscript -e \"rmarkdown::render('/shared/Oocyte_fmndko_spireko_complete.Rmd')\""
 
-mkdir -p $PWD/results/tables
-mv $PWD/notebooks/*.csv $PWD/results/tables
+
 # end message #
 ###############
 end_epoch=`date +%s`
