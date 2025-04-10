4b221a8b703c300a90457fec2f6b012a32f62ec8
andresgordoortiz <andresgordoortiz@gmail.com>
2025-04-10 17:44:02 +0200

    Update full processing pipeline script: add job dependencies for alignment and MultiQC steps

--- workflows/full_processing_pipeline_new_data.sh
+++ workflows/full_processing_pipeline_new_data.sh
@@ -22,19 +22,24 @@ fi
 VASTDB_PATH=$1
 
 
-# Third job - fastQC (dependent on second job)
+# Third job - trim & fastQC
 echo "Submitting second job: Trim reads..."
 jid1=$(sbatch $PWD/scripts/bash/new_data/fastqc_multiqc_new_data.sh | tr -cd '[:digit:].')
 echo "...second job ID is $jid1"
 
 # Fourth job - align reads (dependent on second job)
 echo "Submitting third job: Align reads..."
-jid2=$(sbatch $PWD/scripts/bash/new_data/vast_align_new_data.sh $VASTDB_PATH | tr -cd '[:digit:].')
+jid2=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/new_data/vast_align_new_data.sh $VASTDB_PATH | tr -cd '[:digit:].')
 echo "...third job ID is $jid2"
 
 # Fifth job - run vast combine (dependent on fourth job)
 echo "Submitting fifth job: Run vast combine..."
 jid3=$(sbatch --dependency=afterok:$jid2 $PWD/scripts/bash/new_data/vast_combine_new_data.sh $VASTDB_PATH | tr -cd '[:digit:].')
 echo "...fifth job ID is $jid3"
+
+# Third job - Multiqc
+echo "Submitting second job: Multiqc..."
+jid4=$(sbatch --dependency=afterok:$jid1 $PWD/scripts/bash/new_data/multiqc.sh | tr -cd '[:digit:].')
+echo "...second job ID is $jid4"
 
 echo "All jobs submitted!"
