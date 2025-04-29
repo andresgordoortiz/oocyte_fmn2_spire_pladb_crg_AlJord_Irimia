920cc44ba4fbaec7c3dd3b1501dfc5f875049ed0
andresgordoortiz <andresgordoortiz@gmail.com>
2025-04-29 10:46:35 +0200

    Add new script for downloading data with SLURM settings

---
+++ scripts/bash/download_new_data.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##################
+# slurm settings #
+##################
+
+# where to put stdout / stderr
+#SBATCH --output=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.out
+#SBATCH --error=/users/aaljord/agordo/git/24CRG_ADEL_MANU_OOCYTE_SPLICING/logs/%x.%A_%a.err
+
+# time limit in minutes
+#SBATCH --time=300
+
+# queue
+#SBATCH --qos=short
+#SBATCH --requeue
+
+# memory (MB)
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=8
+
+# job name
+#SBATCH --job-name kallisto-index
+
+#################
+# start message #
+#################
+start_epoch=`date +%s`
+echo [$(date +"%Y-%m-%d %H:%M:%S")] starting on $(hostname)
+
+##################################
+# make bash behave more robustly #
+##################################
+set -e
+set -u
+set -o pipefail
