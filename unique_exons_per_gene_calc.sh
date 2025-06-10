1c6c60f13ce330ae02504ee081d8fd5cb0ca51b1
andresgordoortiz <andresgordoortiz@gmail.com>
2025-06-10 11:21:01 +0200

    Refactor unique_exons_per_gene_calc script to extract gene_name instead of gene_id for accurate counting of unique exons per gene.

--- unique_exons_per_gene_calc.sh
+++ unique_exons_per_gene_calc.sh
@@ -2,18 +2,18 @@ awk '
   BEGIN { FS = "\t"; OFS = "\t" }
   $3 == "exon" &&
   ($9 ~ /gene_biotype "protein_coding"/ || $9 ~ /gene_type "protein_coding"/) {
-    # extract gene_id
-    match($9, /gene_id "([^"]+)"/, m)
-    gid = m[1]
+    # extract gene_name
+    match($9, /gene_name "([^"]+)"/, m)
+    gname = m[1]
 
     # unique span: chr:start-end
     span = $1 ":" $4 "-" $5
 
-    # composite key: gene_id SUBSEP span
-    key = gid SUBSEP span
+    # composite key: gene_name SUBSEP span
+    key = gname SUBSEP span
 
     if (!seen[key]++) {
-      count[gid]++
+      count[gname]++
     }
   }
   END {
@@ -21,6 +21,6 @@ awk '
       print g, count[g]
     }
   }
-' Mus_musculus.GRCm39.113.gtf \
+' $1 \
 | sort -k2,2nr -k1,1 \
 > coding_unique_exon_counts_per_gene.txt
